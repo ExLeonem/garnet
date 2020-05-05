dd4ae62c6f06cfe4f7e8738e59a93bb27f021583
exleonem <maksim.sandybekov@live.de>
2020-05-05 13:14:25 +0200

    modifiy api routes

--- api_server/src/routes.rs
+++ api_server/src/routes.rs
@@ -37,34 +37,28 @@ pub fn index() -> Html<&'static str> {
     "#)
 }
 
-#[get("/allTrashcans", format="json")]
-pub fn get_all_trashcans() -> JsonValue {
-    let trash_cans = db::select_all_trashcans();
-    let json_object = json!(trash_cans);
-    json_object
+// All trashcans
+#[get("/bin?<filled>&<districtIds>", format="json")]
+pub fn get_all_trashcans(filled: Option<String>, districtIds: Option<String>) -> JsonValue {
+    
+    if (filled != "" && districtIds != "") {
+        let mut vec = Vec::new();
+        for i in 0..d_input.districts.len() {
+            vec.push(d_input.districts[i])
+        }
+        let result = db::select_filled_trashcans_from_districts(vec);
+        let trashcans_json = json!(result);
+        trashcans_json
+
+    } else {
+        let trash_cans = db::select_all_trashcans();
+        let json_object = json!(trash_cans);
+        json_object
+
+    }
 }
 
-#[get("/trashCan/<id>", format="json")]
-pub fn trashcan(id: i32) -> JsonValue {
-    let trashcan = db::select_trashcan(id);
-    let json_object = json!(trashcan);
-    json_object
-}
-
-#[get("/allDistricts", format="json")]
-pub fn get_all_districts() -> JsonValue {
-    let disctricts = db::select_all_districts();
-    let disctricts_json = json!(disctricts);
-    disctricts_json
-} 
-
-#[get("/allFilledDistricts", format="json")]
-pub fn get_all_filled_districts() -> JsonValue {
-    let result = db::select_filled_district_ids();
-    let trashcans_json = json!(result);
-    trashcans_json
-} 
-
+// Replace with above endpoint
 #[post("/getFilledTrashcans", data="<d_input>", format="json")]
 pub fn get_filled_trashcans(d_input: Json<DistrictsInput>) -> JsonValue {
     let mut vec = Vec::new();
@@ -75,6 +69,44 @@ pub fn get_filled_trashcans(d_input: Json<DistrictsInput>) -> JsonValue {
     let trashcans_json = json!(result);
     trashcans_json
 }
+
+// Create a new trashcan via endpoint
+#[post("/bin", data="<trashcan>", format="json")]
+pub fn add_trashcan(trashcan: Json<NewTrashcan>) -> () {
+    println!("trashcan fillweight: {:?}", trashcan.fill_weight);
+    let tc : NewTrashcan = NewTrashcan {
+        district: trashcan.district,
+        longitude: trashcan.longitude,
+        latitude: trashcan.latitude,
+        fill_weight : trashcan.fill_weight,
+        trashtype: trashcan.trashtype
+    };
+    db::insert_trashcan(tc);
+}
+
+// Update a trashcan values
+ #[patch("/bin/<id>", data = "<input>", format="json")]
+ pub fn fill_trashcan(id: i32, input: Json<Input>) -> () {
+    println!("fill can: {:?} with value: {:?}", input.id, input.fill_weight);
+    db::update_trashcan_fill_weight(input.id, input.fill_weight);
+}
+
+// Specific information about a single trashcan
+#[get("/bin/<id>", format="json")]
+pub fn trashcan(id: i32) -> JsonValue {
+    let trashcan = db::select_trashcan(id);
+    let json_object = json!(trashcan);
+    json_object
+}
+
+// Districts known to the system filled or not filled
+#[get("/district?<filled>", format="json")]
+pub fn get_all_districts(filled: Option<String>) -> JsonValue {
+    let disctricts = db::select_all_districts();
+    let disctricts_json = json!(disctricts);
+    disctricts_json
+} 
+
 
 #[get("/optimalPath", data="<d_input>", format="json")]
 pub fn get_optimal_path(d_input: Json<DistrictsInput>) -> JsonValue {
@@ -90,24 +122,6 @@ pub fn get_optimal_path(d_input: Json<DistrictsInput>) -> JsonValue {
     trashcans_json
 }
 
-#[post("/trashcan", data="<trashcan>", format="json")]
-pub fn add_trashcan(trashcan: Json<NewTrashcan>) -> () {
-    println!("trashcan fillweight: {:?}", trashcan.fill_weight);
-    let tc : NewTrashcan = NewTrashcan {
-        district: trashcan.district,
-        longitude: trashcan.longitude,
-        latitude: trashcan.latitude,
-        fill_weight : trashcan.fill_weight,
-        trashtype: trashcan.trashtype
-    };
-    db::insert_trashcan(tc);
-}
-
- #[post("/fillTrashcan", data = "<input>", format="json")]
- pub fn fill_trashcan(input: Json<Input>) -> () {
-    println!("fill can: {:?} with value: {:?}", input.id, input.fill_weight);
-    db::update_trashcan_fill_weight(input.id, input.fill_weight);
-}
 
 #[post("/updateTrashcan", data = "<district_input>", format="json")]
 pub fn update_trashcan(district_input: Json<District>) -> () {
